be9c67c6ad2b211ea0254c15a620cd5594f57a17
Anna Teilane <kastekastee@gmail.com>
2024-10-13 19:31:51 +0000

    test: open and read values from txt file

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+# Import ASCII font art library
 from pyfiglet import figlet_format
-text = input("Enter your text: ")
-print(figlet_format(text, font='standard'))
+
+figlet = figlet_format
+# text = input("Enter your text: ")
+# print(figlet(text, font='standard'))
+
+with open('categories.txt', 'r') as file:
+    categories = file.read().splitlines()
+    print(categories)
