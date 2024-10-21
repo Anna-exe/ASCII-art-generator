39a130d4d877c27d24df7a540cc345345bafc272
Anna Teilane <kastekastee@gmail.com>
2024-10-21 14:22:44 +0000

    py: add font categories dictionary

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 # Import ASCII font art library
 import pyfiglet
 
+# Categorizing font styles
+font_categories = {
+    "Basic": ["standard", "slant", "banner", "doom"],
+    "Script": ["script", "dancing", "nvscript", "starwars"],
+    "Fancy": ["3d", "big", "block", "puffy"],
+    "Comic": ["cheese", "cyberlarge", "ghost", "larry3d"],
+    "My Favourites": ["poison", "smkeyboard", "fraktur", "katakana", "mono12"]
+}
+
 def main():
 
     print("Welcome to text art generator!")
